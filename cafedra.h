54be7152def16f8fbc1b9dc090e8706c63b6756c
Michail5647 <mim4lyshev@gmail.com>
2026-03-03 10:32:59 +0300

    test3

--- cafedra.h
+++ cafedra.h
@@ -4,29 +4,25 @@
 #include <iostream>
 
 class Group {
-private:
+ private:
     char* name;
     int count;
     static inline int total_count = 0;
     int my_id;
-public:
-    Group();
 
+ public:
+    Group();
 
     Group(const char* n, int c);
 
-
     Group(const Group& other);
 
-
     ~Group();
-
 
     Group& operator=(const Group& other);
 
     friend std::ostream& operator<<(std::ostream& os, const Group& group);
     friend std::istream& operator>>(std::istream& is, Group& group);
-
 };
 
 #endif
