a126848df4e1d766df4bf255d6fc3ccc85be39f9
Michail5647 <mim4lyshev@gmail.com>
2026-03-03 10:25:08 +0300

    test1

---
+++ cafedra.cpp
@@ -0,0 +1,72 @@
+#include "cafedra.h"
+#include <cstring>
+
+Group::Group() {
+    name = new char[1];
+    name[0] = '\0';
+    count = 0;
+
+    my_id = ++total_count;
+    std::cout << "Создан объект:" << my_id << "\n";
+}
+
+
+Group::Group(const char* n, int c) {
+    name = new char[strlen(n) + 1];
+    strcpy(name, n);
+    count = c;
+
+    my_id = ++total_count;
+    std::cout << "Создан объект:" << my_id << " (Группа: " << name << ")" << "\n";
+
+}
+
+
+Group::Group(const Group& other) {
+    count = other.count;
+    name = new char[strlen(other.name) + 1];
+    strcpy(name, other.name);
+
+    my_id = ++total_count;
+    std::cout << "Создан объект:" << my_id << " (КОПИЯ объекта :" << other.my_id << ")" << "\n";
+}
+
+Group::~Group() {
+    delete[] name;
+    --total_count;
+    std::cout << "Удален объект :" << my_id
+         << " (Осталось в памяти: " << total_count << ")" << "\n";
+}
+
+
+
+Group& Group::operator=(const Group& other) {
+    if (this != &other) {
+        delete[] name;
+
+        count = other.count;
+        name = new char[strlen(other.name) + 1];
+        strcpy(name, other.name);
+    }
+    return *this;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Group& group) {
+    os << "Группа: " << group.name << ", Студентов: " << group.count;
+    return os;
+}
+
+
+std::istream& operator>>(std::istream& is, Group& group) {
+    char buffer[256];
+    is >> buffer >> group.count;
+
+    delete[] group.name;
+
+
+    group.name = new char[strlen(buffer) + 1];
+    strcpy(group.name, buffer);
+
+    return is;
+}
---
+++ maincafedra.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include "cafedra.h"
+
+
+int main() {
+
+    Group** cafedra = nullptr;
+    int groupCount = 0;
+
+    Group* tempGroup = nullptr;
+
+    int choice = 0;
+    while (choice != 5) {
+        std::cout << "\n МЕНЮ КАФЕДРЫ \n";
+        std::cout << "1. Создать объект (ввод названия группы и числа студентов)\n";
+        std::cout << "2. Вывод текущего (временного) объекта\n";
+        std::cout << "3. Добавить объект в динамический массив кафедры\n";
+        std::cout << "4. Вывод массива кафедры\n";
+        std::cout << "5. Выход из программы\n";
+        std::cout << "Выберите действие: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                if (tempGroup != nullptr) {
+                    delete tempGroup;
+                }
+                tempGroup = new Group();
+                std::cout << "Введите название группы (без пробелов) и количество студентов:\n";
+                std::cin >> *tempGroup;
+                std::cout << "Объект успешно создан!\n";
+                break;
+            }
+            case 2: {
+                if (tempGroup != nullptr) {
+
+                    std::cout << "Текущий объект -> " << *tempGroup << "\n";
+                } else {
+                    std::cout << "Объект еще не создан! Сначала выполните пункт 1.\n";
+                }
+                break;
+            }
+            case 3: {
+                if (tempGroup != nullptr) {
+
+                    Group** newCafedra = new Group*[groupCount + 1];
+
+                    for (int i = 0; i < groupCount; ++i) {
+                        newCafedra[i] = cafedra[i];
+                    }
+
+
+                    newCafedra[groupCount] = new Group(*tempGroup);
+
+
+                    delete[] cafedra;
+
+
+                    cafedra = newCafedra;
+                    groupCount++;
+
+                    std::cout << "Объект успешно добавлен на кафедру!\n";
+                } else {
+                    std::cout << "Сначала создайте объект (пункт 1)!\n";
+                }
+                break;
+            }
+            case 4: {
+                if (groupCount == 0) {
+                    std::cout << "Кафедра пуста.\n";
+                } else {
+                    std::cout << "\n Список групп на кафедре \n";
+                    for (int i = 0; i < groupCount; ++i) {
+
+                        std::cout << i + 1 << ". " << *(cafedra[i]) << "\n";
+                    }
+                }
+                break;
+            }
+            case 5: {
+                std::cout << "Выход из программы\n";
+                break;
+            }
+            default: {
+                std::cout << "Неверный ввод. Попробуйте снова (цифры от 1 до 5).\n";
+                break;
+            }
+        }
+    }
+
+
+
+    if (tempGroup != nullptr) {
+        delete tempGroup;
+    }
+
+    if (cafedra != nullptr) {
+        for (int i = 0; i < groupCount; ++i) {
+            delete cafedra[i];
+        }
+        delete[] cafedra;
+    }
+    
+    return 0;
+
+}
